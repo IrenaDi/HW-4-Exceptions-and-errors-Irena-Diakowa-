f2c6cf7830f2638400ba19c08f0b1291ad11ae94
IrenaDi <110069875+IrenaDi@users.noreply.github.com>
2022-08-07 12:55:53 +0200

    Update Calculator.java

--- src/com/Calculator.java
+++ src/com/Calculator.java
@@ -1,5 +1,33 @@
 package com;
-
-public class Calculator {
-
+public class Calculator  extends Input implements Contact {
+    int firstNumber = firstNumberInput();
+    char mathOperand = getMathOperator();
+    int secondNumber = secondNumberInput();
+    public void getCalculated() {
+        float result = 0.00f;
+        switch (mathOperand) {
+            case '+':
+                result = firstNumber + secondNumber;
+                System.out.println(outcome+result);
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                System.out.println(outcome+result);
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                System.out.println(outcome+result);
+                break;
+            case '/':
+                if (secondNumber == 0) {
+                    System.out.println(divisionZero);
+                } else {
+                    result = (float) firstNumber / secondNumber;
+                    System.out.println(outcome+result);
+                }
+                break;
+            default:
+                System.out.println(mistake);
+        }
+    }
 }
