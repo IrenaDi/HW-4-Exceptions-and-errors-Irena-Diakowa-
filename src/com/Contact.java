21ef20a87e51b240fd1f8800ce7ac7ab3dff8db6
IrenaDi <110069875+IrenaDi@users.noreply.github.com>
2022-08-07 12:59:17 +0200

    Create Contact.java

---
+++ src/com/Contact.java
@@ -0,0 +1,9 @@
+package com;
+public interface  Contact  {
+     String outcome =("Твій результат : ");
+     String mistake =("Сонечко, щось пішло не так... Спробуй знову!");
+     String divisionZero =("О, ні! На нуль ділити не можна!");
+     String firstNumberInput=("Введи перше число: ");
+     String getMathOperatorInput=("Вибери математичну дію: + або - або * або /");
+     String secondNumberInput=("Введи друге число: ");
+}
